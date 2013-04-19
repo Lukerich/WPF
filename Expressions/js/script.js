51c1724f697e4afff67c7f51bda2ba24b0ade41a
Richard Luke <r.l.luke@fullsail.edu>
2013-04-19 00:01:06 -0400

    Expressions

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,7 +1,7 @@
 //Expressions   - Assignment Operators
 
 var a = 3;
-// a ++;   // a = a + 1 // a += 1
-a --; // a = a - 1 // a-=1
+a ++;   // a = a + 1 // a += 1
+b --; // b = b - 1 // b-=1
 console.log(a);
 
