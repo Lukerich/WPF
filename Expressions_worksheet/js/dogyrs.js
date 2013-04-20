c9b72f8416177635b29080d5914274f226943866
Richard Luke <r.l.luke@fullsail.edu>
2013-04-20 01:37:58 -0400

    Expressions_worksheet

--- Expressions_worksheet/js/dogyrs.js
+++ Expressions_worksheet/js/dogyrs.js
@@ -6,8 +6,10 @@
 
 // Calculate how old Sparky the pit bull is in dogs years based on his actual age
 
-var sparkyAge = 1;
+var sparkyAge = 1;  // Variable assigning Sparky' actual age
 
-var sparkyDY = sparkyAge * 7;
-
+var sparkyDY = sparkyAge * 7; /** Variable assigning Sparky' dog years to the calculation of the variable
+                               *  for Sparky' actual age times the constant of 7
+                               */
+// Printing out the output of Sparky' dog years
 console.log("Sparky is " + [sparkyAge] + " human years old which is " + [sparkyDY] + " in dog years.");
