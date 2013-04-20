da0377e4d6fad5e49375bc78fa1f9e560da4fdd9
Richard Luke <r.l.luke@fullsail.edu>
2013-04-20 00:36:32 -0400

    Expressions_worksheet

---
+++ Expressions_worksheet/js/avgshbill.js
@@ -0,0 +1,5 @@
+/**
+ * Richard Luke
+ * Date: 4/20/13
+ * Expressions Worksheet - Average shopping bill
+ */
---
+++ Expressions_worksheet/js/slcpie2.js
@@ -0,0 +1,5 @@
+/**
+ * Richard Luke
+ * Date: 4/20/13
+ * Expressions Worksheet - Slice of Pie part II
+ */
