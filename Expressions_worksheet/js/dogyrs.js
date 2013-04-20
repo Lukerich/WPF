0eebe7153a63f94005530a940330e2c805d3e4e1
Richard Luke <r.l.luke@fullsail.edu>
2013-04-20 00:44:26 -0400

    Expressions_worksheet

--- Expressions_worksheet/js/dogyrs.js
+++ Expressions_worksheet/js/dogyrs.js
@@ -1,7 +1,5 @@
 /**
- * Created with JetBrains WebStorm.
- * User: rluke
+ * Richard Luke
  * Date: 4/20/13
- * Time: 12:15 AM
- * To change this template use File | Settings | File Templates.
+ * Expressions Worksheet - Dog Years
  */
