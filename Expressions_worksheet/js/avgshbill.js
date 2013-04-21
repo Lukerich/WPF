cac1a63d927d061595134001244e8fae9562cf7c
Richard Luke <r.l.luke@fullsail.edu>
2013-04-20 23:30:18 -0400

    Expressions_worksheet

--- Expressions_worksheet/js/avgshbill.js
+++ Expressions_worksheet/js/avgshbill.js
@@ -8,14 +8,14 @@
 
 var lst5Wkbill = [221.23, 305.29, 250.12, 288.64, 300.31];  // An array of the last five weekly grocery totals
 
-a = lst5Wkbill[0]; // assigning value of 1st position of array
-b = lst5Wkbill[1]; // assigning value of 2nd position of array
-c = lst5Wkbill[2]; // assigning value of 3rd position of array
-d = lst5Wkbill[3]; // assigning value of 4th position of array
-e = lst5Wkbill[4]; // assigning value of 5th position of array
+//a = lst5Wkbill[0]; // assigning value of 1st position of array
+//b = lst5Wkbill[1]; // assigning value of 2nd position of array
+//c = lst5Wkbill[2]; // assigning value of 3rd position of array
+//d = lst5Wkbill[3]; // assigning value of 4th position of array
+//e = lst5Wkbill[4]; // assigning value of 5th position of array
 
 
-var totGB = a+b+c+d+e; // assigning variable totGB to the sum of a through e
+var totGB = lst5Wkbill[0] + lst5Wkbill[1] + lst5Wkbill[2] + lst5Wkbill[3] + lst5Wkbill[4]; // assigning variable totGB to the sum the array
 
 // var totGB = lst5Wkbill[0] + lst5Wkbill[1] + lst5Wkbill[2] + lst5Wkbill[3] + lst5Wkbill[4];  //Total bill for the last five weeks of groceries
 
--- Expressions_worksheet/js/discounts.js
+++ Expressions_worksheet/js/discounts.js
@@ -3,3 +3,16 @@
  * Date: 4/20/13
  * Expressions Worksheet - Discounts
  */
+// Calculate discounted price for an item
+
+var orignPrc = 50.55;  // Original price of item
+var discPrc = 15 ;  //  Discount percentage
+var discpt =  "Fly reel" ; // Description of item
+var sTaxPrc =  8 ; // Sales tax percentage
+
+    var prcWtax = orignPrc * discPrc + prcWoTax * sTaxPrc; // Price of item with tax
+    var prcWoTax = orignPrc * discPrc ; // Price of item without tax
+
+
+// Printing out the output of the item it's original price, the discount percentage, and the with and without tax prices
+console.log("Your " + [discpt] + " was originally " + "$" + [orignPrc] + ", but after a " + [discPrc] + "%" + " discount, it is now " + "$" + [prcWoTax] + " without tax, and " + "$" + [prcWtax] + " with tax");
