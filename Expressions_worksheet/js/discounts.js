17e2aa0e71bb959b4d6764b7c81dfdbed187cf27
Richard Luke <r.l.luke@fullsail.edu>
2013-04-20 23:55:50 -0400

    Expressions_worksheet

--- Expressions_worksheet/js/discounts.js
+++ Expressions_worksheet/js/discounts.js
@@ -6,14 +6,14 @@
 // Calculate discounted price for an item
 
 var orignPrc = 50.55;  // Original price of item
-var discPrc = 15;  //  Discount percentage
+var discPrc = [15,.15];  //  Discount percentage
 var discpt =  "Fly reel"; // Description of item
-var sTaxPrc =  8; // Sales tax percentage
+var sTaxPrc =  .08; // Sales tax percentage
 
-    var prcWoTax = orignPrc * discPrc ; // Price of item without tax
-    var prcWtax = orignPrc * discPrc + prcWoTax * sTaxPrc; // Price of item with tax
+    var prcWoTax = orignPrc - orignPrc * discPrc[1] ; // Price of item without tax
+    var prcWtax = orignPrc - orignPrc * discPrc[1] + prcWoTax * sTaxPrc; // Price of item with tax
 
 
 
 // Printing out the output of the item it's original price, the discount percentage, and the with and without tax prices
-console.log("Your " + [discpt] + " was originally " + "$" + [orignPrc] + ", but after a " + [discPrc] + "%" + " discount, it is now " + "$" + [prcWoTax] + " without tax, and " + "$" + [prcWtax] + " with tax");
+console.log("Your " + [discpt] + " was originally " + "$" + [orignPrc] + ", but after a " + [discPrc[0]] + "%" + " discount, it is now " + "$" + [prcWoTax] + " without tax, and " + "$" + [prcWtax] + " with tax");
